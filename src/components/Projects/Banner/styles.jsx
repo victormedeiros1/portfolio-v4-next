db18e361169c0d4ec7db3b81a14d923d5d80f52e
victormedeiros1 <josevqczt@gmail.com>
2022-09-10 15:07:41 -0300

    feat: Banner hover animation

--- src/components/Projects/Banner/styles.jsx
+++ src/components/Projects/Banner/styles.jsx
@@ -1,5 +1,11 @@
 import { styled } from '@stitches/react';
 
 export const BannerStyles = styled('img', {
+  cursor: 'pointer',
   transform: 'translateX(-150px) rotate(20deg)',
+  transition: '0.3s',
+
+  '&:hover': {
+    transform: 'translateX(-130px) translateY(5px) rotate(20deg)',
+  },
 });
