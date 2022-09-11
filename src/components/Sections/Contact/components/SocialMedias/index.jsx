2f7c41d9f1e6dfc0c61fa623c374447e1050ac76
victormedeiros1 <josevqczt@gmail.com>
2022-09-11 15:15:08 -0300

    fix: LinkedIn icon component URL

--- src/components/Sections/Contact/components/SocialMedias/index.jsx
+++ src/components/Sections/Contact/components/SocialMedias/index.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import Figma from 'src/components/Icons/Figma';
 import Github from 'src/components/Icons/Github';
 import Instagram from 'src/components/Icons/Instagram';
-import Linkedin from 'src/components/Icons/LinkedIn';
+import LinkedIn from 'src/components/Icons/LinkedIn';
 import { SocialMediasStyles, SocialMedia, SocialMediaGroup } from './styles';
 
 const SocialMedias = () => {
