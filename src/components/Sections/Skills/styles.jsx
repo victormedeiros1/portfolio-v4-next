fe8000285861e0737695dc758ab737c40a1d16ba
victormedeiros1 <josevqczt@gmail.com>
2022-10-17 17:21:52 -0300

    fix: Skills title size and breakpoints

--- src/components/Sections/Skills/styles.jsx
+++ src/components/Sections/Skills/styles.jsx
@@ -2,10 +2,19 @@ import { styled } from '@stitches/react';
 
 export const Title = styled('h2', {
   position: 'absolute',
-  fontSize: '48px',
+  fontSize: '$48',
   left: '50%',
   transform: 'translate(-40%)',
   top: '48%',
   color: '$green',
   zIndex: '$1',
+
+  '@media (max-width: 992px)': {
+    fontSize: '$48',
+  },
+  '@media (max-width: 576px)': {
+    fontSize: '$20',
+    transform: 'translate(-35%)',
+    top: '49%',
+  },
 });
